b0ce8b1a66f9f1cdb95aa82c9a5fcc493dfa9436
gerry <gerry@doocom.cn>
2022-01-09 21:50:29 +0800

    feat: add export

---
+++ scripts/config.js
@@ -0,0 +1,6 @@
+// const config = {
+//     userType: 'student',
+//     name: 'myName'
+// };
+const config = 'config';
+export default config;
--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,6 @@
+import { default as config } from './config.js'
+
+console.log(config);
+
 let myHeading = document.querySelector('h1');
 myHeading.textContent = '我的偶像 卡卡西!';
